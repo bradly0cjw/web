33e38ec4ec1e14a9e891c5a1c0d73e3d9916cd49
bradly0cjw <bradly093@gmail.com>
2022-08-15 21:09:39 +0800

    upload file

--- readme.md
+++ readme.md
@@ -1,1 +1,33 @@
+This Project is base on
+[](https://github.com/wiwikuan/fast-srt-subtitle)
 
+# Insatall
+
+npm install -g http-server
+npm install --save js-base64
+
+
+# fast-srt-subtitle
+Make SRT Caption Fast!!!! This is a fast and dirty javascript SRT caption tool.
+
+* Controls:
+  * K: Timestamp this line's end time & next line's start time 
+  * L: Timestamp this line's end time
+  * I: Scroll back one line 
+  * O: Scroll forward one line
+  * U: Rewind 2 seconds
+  * P: Forward 2 seconds
+  * Q: Make SRT File
+  * W: Play/Pause
+
+Enjoy.
+
+# FUCK ADOBE PREMIERE PRO CAPTION
+
+
+# Progress
+decode adobe premiere xml (Done)
+
+# TODO
+Convert Srt into Array
+FIX ASCII decode issue
---
+++ test.js
@@ -0,0 +1,54 @@
+
+import { Base64 } from 'js-base64';
+const CHG_ID = 'chgFile';
+const PR_ID = 'PRFile';
+
+const chgInput = document.querySelector('#chgFile');
+const prInput = document.querySelector('#PRFile');
+
+const b64 = document.querySelector('#b64');
+const rep = document.querySelector('#rep');
+const opt = document.querySelector('#opt');
+const inp = document.querySelector('#inp');
+var FUCKADOBE = new Uint8Array([102,15,0,0,0,0,0,0])
+
+function clicked(){
+
+var chgbin = Base64.toUint8Array(b64.value)
+var chgrep = ((chgbin.toString()).replace('102,15,0,0,0,0,0,0,','')).replace(/,0/g,'')
+console.log(chgrep)
+var tranbin = new Uint8Array (chgrep.split(","))
+var tranutf8 = Base64.decode(Base64.fromUint8Array(tranbin))
+var replace = tranutf8.replace('@', rep.value)
+var encode = (Base64.encode(replace))
+var encode2 = new Uint8Array(((Base64.toUint8Array(encode)).join(',,')).split(","))
+var newarray = new Uint8Array(encode2.length+FUCKADOBE.length+1)
+newarray.set(FUCKADOBE)
+newarray.set(encode2,FUCKADOBE.length)
+var encode3 = Base64.fromUint8Array(newarray)
+inp.value=b64.value
+document.getElementById('inp2').value=tranutf8
+document.getElementById('inp3').value=chgbin
+opt.value= encode3
+document.getElementById('opt2').value=replace
+document.getElementById('opt3').value=Base64.toUint8Array(opt.value)
+// if (1===1){
+//     document.getElementById('check').innerHTML="TRUE"   
+// }else{
+//     document.getElementById('check').innerHTML="False"
+// }
+}
+
+// function clicked2(){
+//     const decode = document.getElementById('inp2').value
+//     document.getElementById('enc').value=Base64.encode(decode)
+//     document.getElementById('enc2').value=Base64.toUint8Array(document.getElementById('enc').value)
+
+// }
+// function clicked3(){
+//     const decode = new Uint8Array(document.getElementById('bin').value.split(","))
+//     console.log(decode)
+//     document.getElementById('bin2').value=Base64.fromUint8Array(decode)
+    
+
+// }
